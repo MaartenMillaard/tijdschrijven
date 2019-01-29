6e92b72814f7dfaf643776fdc3b688f84b6af97e
Maarten Millaard <Maarten.Millaard@planet.nl>
2019-01-29 18:44:37 +0100

    herstel

--- src/main/java/nl/gemeente/groningen/tijdschrijven/Tijdschrijven.java
+++ src/main/java/nl/gemeente/groningen/tijdschrijven/Tijdschrijven.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Tijdschrijven {
 
-	@Autowired
 	public static void main(String[] args) {
 		System.out.println("Starting application...");
 
